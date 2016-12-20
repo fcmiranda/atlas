452784be8db191b940cfbbd1db487f055afd0a97
felipe <felip.3@hotmail.com>
2016-12-20 04:02:33 -0200

    atlas 1.0

---
+++ public/app/enter/ng-enter.directive.js
@@ -0,0 +1,12 @@
+app.directive('ngEnter', function () {
+    return function (scope, elements, attrs) {
+        elements.bind('keydown keypress', function (event) {
+            if (event.which === 13) {
+                scope.$apply(function () {
+                    scope.form.$setSubmitted();
+                });
+                event.preventDefault();
+            }
+        });
+    };
+});
